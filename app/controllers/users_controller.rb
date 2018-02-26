f0924eff373ff5ad4f89f99a84449b5060bb0313
Connor Ingold <connoringold@gmail.com>
2018-02-26 14:41:33 +0000

    added controllers for users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :create, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
+
+end
