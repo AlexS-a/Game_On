b6fb836ca118610fe4857b2c8b6ff37bd6f05fed
Alexander Sips-Avello <alex.s.a1994@gmail.com>
2018-02-26 16:53:15 +0000

    Added a new and create view and controller to the games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,6 +7,21 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
   end
 
+  def new
+    @game = Game.new
+  end
 
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
 
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :description, :genre, :location, :platform, :price)
+  end
 end
