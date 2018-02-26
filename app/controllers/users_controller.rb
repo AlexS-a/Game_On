c3db8342b7703b5ed9744f3a0ea958da3b74610c
Sumai Khayat <sumaikhayat@gmail.com>
2018-02-26 20:25:05 +0000

    Started the user controller methods for show, edit, update and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to users_path(@user)
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to root
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
+
