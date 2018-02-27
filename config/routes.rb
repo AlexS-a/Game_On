6e57df42d4db3d62380795b2c38c0ee0790e39bf
Samuel Gasper <1516828@my.brunel.ac.uk>
2018-02-27 10:49:59 +0000

    Done with all changes that broke the app and i'm on Footer this time

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :games do
     # resources :bookings, only: [:index, :show, :new, :create, :edit, :destroy]
-    resources :bookings, except: [:update]
+    resources :bookings, except: [:index, :show]
   end
+
+  resources :bookings, only: [:index, :show]
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
