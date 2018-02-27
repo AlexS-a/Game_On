056182120fbc4caea1bbfc13f145b4f2874f391f
Samuel Gasper <1516828@my.brunel.ac.uk>
2018-02-27 10:47:49 +0000

    Done with all changes that broke the app before. Fixed routes as well

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :games do
-    resources :bookings, only: [:index, :show, :new, :create, :edit, :destroy]
+    resources :bookings, except: [:show, :index]
   end
+
+  resources :bookings, only: [:index, :show]
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
