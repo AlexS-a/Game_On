e24ddab8720e063ecc6bc64a61895c5a303289c8
Alexander Sips-Avello <alex.s.a1994@gmail.com>
2018-03-02 12:36:54 +0000

    I fucking did

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   resources :bookings, only: [:index, :show, :destroy]
   root to: 'pages#home'
-
+  get 'bookings/:id/accept' => 'bookings#status_accept', as: 'accept'
+  get 'bookings/:id/decline' => 'bookings#status_decline', as: 'decline'
   resources :users, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
